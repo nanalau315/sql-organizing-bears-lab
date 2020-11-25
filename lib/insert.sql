ac35a667bc534a839568fca5b2c7f246c04a25c7
Nana Lau <nanalau315@hotmail.com>
2020-11-24 20:56:36 -0500

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 4, "M", "Brown", "Good", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 6, "M", "Black", "Bad", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 5, "F", "Purple", "Good", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 4, "M", "Pink", "Good", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 5, "F", "Blue", "Bad", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 4, "M", "Orange", "Good", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 4, "F", "Red", "Bad", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, NULL, "M", "Green", "Bad", 1)
